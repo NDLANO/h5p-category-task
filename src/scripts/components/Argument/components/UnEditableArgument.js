6ac6d452bab9e444e40b09d88563481a3018fa6d
Oliver Tacke <o.tacke@posteo.de>
2026-02-19 15:03:51 +0100

    Fix draggable sizing

--- src/scripts/components/Argument/components/UnEditableArgument.js
+++ src/scripts/components/Argument/components/UnEditableArgument.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './UnEditableArgument.scss';
+
 const UnEditableArgument = ({ argument }) => (
   <p className={'h5p-category-task-element'}>
     {argument}
